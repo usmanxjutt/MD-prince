1d1258189e45ba0fc6f6f0277a5dbe567cfc20d8
usmanxjutt <sounogggg@gmail.com>
2025-02-13 19:46:04 +0500

    config.js

--- config.js
+++ config.js
@@ -22,9 +22,9 @@ global.APIKeys = {
 }
 
 //═══════[modification]════════\\
-global.owner = ['918590565968','919446652312'] //owner number, u can put multiple owner number, within quotations and seperated by comma.
-global.pemilik = ['918590565968'] //another owner number
-global.premium = ['918590565968'] //premium number
+global.owner = ['923071593851','919446652312'] //owner number, u can put multiple owner number, within quotations and seperated by comma.
+global.pemilik = ['923071593851'] //another owner number
+global.premium = ['923071593851'] //premium number
 global.pengguna = 'prince' //username
 global.botnma = 'MD-prince' //bot name
 global.ownernma = 'prince ser' //owner name
